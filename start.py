f33ff63a898b025e8be189a7ec4a015a8ed7bd5a
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-12-21 23:33:19 +0330

    test for change

--- start.py
+++ start.py
@@ -4,10 +4,10 @@ import os
 from colorama import Fore,init
 import subprocess
 
-c = input(Fore.GREEN+' [ + ] '+Fore.WHITE+'Please Enter your comment : ')
+c = input(Fore.GREEN+' [ + ] '+Fore.WHITE+'Please Enter your comment for commit : ')
 
 add = os.system('git add -A')
 comment = os.system(f'git commit -m "{ c }"')
 status = os.system('git status')
-if comment == True:
+if True:
     print(Fore.GREEN+"Commited by this commit ==> "+Fore.WHITE+comment)
