ac043ff70489fd67dbc13fe8408af5cfb3506008
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-12-21 23:29:11 +0330

    make a change

--- start.py
+++ start.py
@@ -1,3 +1,13 @@
+# start the coding automate git cycle 
+# import library's
 import os
-import colorama
+from colorama import Fore,init
 import subprocess
+
+c = input(Fore.GREEN+' [ + ] '+Fore.WHITE+'Please Enter your comment : ')
+
+add = os.system('git add -A')
+comment = os.system(f'git commit -m "{ c }"')
+status = os.system('git status')
+
+print(Fore.GREEN+"Commited by this commit ==> "+Fore.WHITE+comment)
