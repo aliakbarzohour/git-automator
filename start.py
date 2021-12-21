4522803ca1f8e9699453bc8ca7af08957ddd306b
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-12-21 23:30:24 +0330

    make a change

--- start.py
+++ start.py
@@ -9,5 +9,5 @@ c = input(Fore.GREEN+' [ + ] '+Fore.WHITE+'Please Enter your comment : ')
 add = os.system('git add -A')
 comment = os.system(f'git commit -m "{ c }"')
 status = os.system('git status')
-
-print(Fore.GREEN+"Commited by this commit ==> "+Fore.WHITE+comment)
+if comment == True:
+    print(Fore.GREEN+"Commited by this commit ==> "+Fore.WHITE+comment)
